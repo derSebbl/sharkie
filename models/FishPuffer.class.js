90c3d5703f8af674ae6d7bf11c6822998204708d
Sebastian <139996363+derSebbl@users.noreply.github.com>
2023-11-08 09:54:22 +0100

    Making puff transition

--- models/FishPuffer.class.js
+++ models/FishPuffer.class.js
@@ -15,20 +15,60 @@ class FishPuffer extends movableObject {
         'img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/3.swim5.png',
     ];
 
+    IMAGES_TRANSITION = [
+        'img/2.Enemy/1.Puffer fish (3 color options)/2.transition/3.transition1.png',
+        'img/2.Enemy/1.Puffer fish (3 color options)/2.transition/3.transition2.png',
+        'img/2.Enemy/1.Puffer fish (3 color options)/2.transition/3.transition3.png',
+        'img/2.Enemy/1.Puffer fish (3 color options)/2.transition/3.transition4.png',
+        'img/2.Enemy/1.Puffer fish (3 color options)/2.transition/3.transition5.png',
+        'img/2.Enemy/1.Puffer fish (3 color options)/2.transition/3.transition5.png',
+        
+    ];
+
+    IMAGES_PUFFED = [
+        'img/2.Enemy/1.Puffer fish (3 color options)/3.Bubbleeswim/3.bubbleswim1.png',
+        'img/2.Enemy/1.Puffer fish (3 color options)/3.Bubbleeswim/3.bubbleswim2.png',
+        'img/2.Enemy/1.Puffer fish (3 color options)/3.Bubbleeswim/3.bubbleswim3.png',
+        'img/2.Enemy/1.Puffer fish (3 color options)/3.Bubbleeswim/3.bubbleswim4.png',
+        'img/2.Enemy/1.Puffer fish (3 color options)/3.Bubbleeswim/3.bubbleswim5.png',
+    ];
+
     constructor() {
         super().loadImg('img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/3.swim1.png');
         this.loadImages(this.IMAGES_SWIM);
+        this.loadImages(this.IMAGES_TRANSITION);
+        this.loadImages(this.IMAGES_PUFFED);
 
         this.animate();
 
-        this.x = 300 + Math.random() * 3000;
+        this.x = 300 + Math.random() * 4000;
         this.y = 0 + Math.random() * 350;
         this.speed = 1 + Math.random() * 0.25; 
     }
 
     animate() {
+        let e = 0
         setInterval(() => {
+            if(e > 17 && e < 20){
+                this.playAnimation(this.IMAGES_TRANSITION);
+            }
+
+           else if(e > 20 && e < 45){
+                this.playAnimation(this.IMAGES_PUFFED);
+            } 
+
+            else if(e > 67 && e < 70){
+                this.playAnimation(this.IMAGES_TRANSITION);
+            }
+
+            else if (e > 70 && e < 95){
+                this.playAnimation(this.IMAGES_PUFFED);
+            } 
+
+            else {
             this.playAnimation(this.IMAGES_SWIM);
+            }
+            e++
     }, 280)
     setInterval(() => {
         this.moveLeft();
